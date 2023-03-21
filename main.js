96557e138ff93441f321065d3fa7800ff9bf62f0
Arro <103262516+AruneeThongkrathok@users.noreply.github.com>
2023-03-21 20:55:55 +0100

    added functions to log the json objects

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+async function getJson (url){
+  let rawData = await fetch(url);
+  let data = await rawData.json();
+  return data;
+}
 
+async function start(){
+  let books = await getJson('/json/books.json')
+  console.log(books)
+  
+}
+start()
