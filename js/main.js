e49a6bce244025b0cf06d511f41fde0b09acf426
Arro <103262516+AruneeThongkrathok@users.noreply.github.com>
2023-03-22 13:46:57 +0100

    added a funtion that sorts books by title, price and author

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ async function start(){
   let books = await getJSON('/json/books.json')
 
   displayBooks(books);
+  sortBooks(books, 'price-asc');
   
   
 }
@@ -29,4 +30,29 @@ function displayBooks(books){
   document.querySelector('main').innerHTML = html;
 
 }
+
+function sortBooks (books, sortBy){
+
+  if (sortBy === 'title-asc'){
+    books.sort((a,b) => a.title.localeCompare(b.title));
+  }
+  else if (sortBy === 'title-desc'){
+    books.sort((a,b) => b.title.localeCompare(a.title));
+  }
+  else if (sortBy === 'price-asc'){
+    books.sort((a,b) => a.price - b.price);
+  }
+  else if (sortBy === 'price-desc'){
+    books.sort((a,b) => b.price - a.price);
+  }
+  else if (sortBy === 'author-asc'){
+    books.sort((a,b) => a.author.localeCompare(b.author));
+  }
+  else if (sortBy === 'author-desc'){
+    books.sort((a,b) => b.author.localeCompare(a.author));
+  }
+  displayBooks(books);
+
+}
+
 start()
