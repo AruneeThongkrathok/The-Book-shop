e46328fbd6192dc654d3747ab777eec84112aa4b
Arro <103262516+AruneeThongkrathok@users.noreply.github.com>
2023-04-03 17:37:46 +0200

    added filter function to filter after what it is typed in search bar

---
+++ js/filterBooks.js
@@ -0,0 +1,16 @@
+import { displayBooks } from "./displayBooks";
+
+export function filterBooks(books, searchText) {
+  let filteredBooks = books.filter((book) => {
+    const regex = new RegExp(searchText, "gi");
+    return (
+      regex.test(book.title) ||
+      regex.test(book.author) ||
+      regex.test(book.category) ||
+      (book.price >= parseFloat(searchText.split("-")[0]) &&
+        book.price <= parseFloat(searchText.split("-")[1]))
+    );
+  });
+
+  displayBooks(filteredBooks);
+}
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { getJSON } from "./utils/getJSON";
 import { displayBooks } from "./displayBooks";
 import { sortBooks } from "./sortBooks";
-
+import { filterBooks } from "./filterBooks";
 async function start() {
   let books = await getJSON('/json/books.json')
 
@@ -10,6 +10,13 @@ async function start() {
     const sortBy = document.getElementById("sort-select").value;
     sortBooks(books, sortBy);
   });
+
+  document.getElementById("book-filter").addEventListener("input", () => {
+    const filterValue = document.getElementById("book-filter").value;
+    filterBooks(books, filterValue);
+  });
+
+
 }
 
 start();
